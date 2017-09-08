f04c6b2c2b3d908d468ab7fc75b53d7823d273d0
Jonathan Easterman <jonathaneasterman@gmail.com>
2017-09-08 14:25:45 -0700

    Scaffold tests

---
+++ hash_test.go
@@ -0,0 +1,44 @@
+package hash
+
+import "testing"
+
+
+func TestNew(*testing.T) {
+
+}
+
+func TestSetNew(*testing.T) {
+
+}
+
+func TestSetAndUpdate (*testing.T) {
+
+}
+
+func TestSetNewFull (*testing.T) {
+
+}
+
+func TestSetAndUpdateFull (*testing.T) {
+
+}
+
+func TestGetExists(*testing.T) {
+
+}
+
+func TestGetNotExists(*testing.T) {
+
+}
+
+func TestDeleteExists(*testing.T) {
+
+}
+
+func TestDeleteNotExists(*testing.T) {
+
+}
+
+func TestLoad(*testing.T) {
+
+}
