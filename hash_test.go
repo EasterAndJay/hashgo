85fbaaea5574176f7fe78ea3ac4baf5c61048549
Jonathan Easterman <jonathaneasterman@gmail.com>
2017-09-17 11:11:18 -0700

    Include parameter name t for tests

--- hash_test.go
+++ hash_test.go
@@ -3,42 +3,42 @@ package hash
 import "testing"
 
 
-func TestNew(*testing.T) {
+func TestNew(t *testing.T) {
 
 }
 
-func TestSetNew(*testing.T) {
+func TestSetNew(t *testing.T) {
 
 }
 
-func TestSetAndUpdate (*testing.T) {
+func TestSetAndUpdate (t *testing.T) {
 
 }
 
-func TestSetNewFull (*testing.T) {
+func TestSetNewFull (t *testing.T) {
 
 }
 
-func TestSetAndUpdateFull (*testing.T) {
+func TestSetAndUpdateFull (t *testing.T) {
 
 }
 
-func TestGetExists(*testing.T) {
+func TestGetExists(t *testing.T) {
 
 }
 
-func TestGetNotExists(*testing.T) {
+func TestGetNotExists(t *testing.T) {
 
 }
 
-func TestDeleteExists(*testing.T) {
+func TestDeleteExists(t *testing.T) {
 
 }
 
-func TestDeleteNotExists(*testing.T) {
+func TestDeleteNotExists(t *testing.T) {
 
 }
 
-func TestLoad(*testing.T) {
+func TestLoad(t *testing.T) {
 
 }
