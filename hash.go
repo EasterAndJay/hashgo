e0428d89bce3563e41d808a749ae9be952b1cccf
Jonathan Easterman <jonathaneasterman@gmail.com>
2017-09-17 11:39:59 -0700

    Add New test and refactor New

--- hash.go
+++ hash.go
@@ -1,16 +1,9 @@
 package hash
 
-import(
-  "hash/fnv"
+import (
+	"errors"
+	"hash/fnv"
 )
-// Using only primitive types, implement a fixed-size hash map that associates
-// string keys with arbitrary data object references (you don't need to copy the object).
-// Your data structure should be optimized for algorithmic runtime and memory usage.
-// You should not import any external libraries, and may not use primitive hash map
-// or dictionary types in languages like Python or Ruby.
-
-// The solution should be delivered in one class (or your language's equivalent)
-// that provides the following functions:
 
 // constructor(size): return an instance of the class with pre-allocated space for the given number of objects.
 
@@ -22,77 +15,83 @@ import(
 
 // float load(): return a float value representing the load factor (`(items in hash map)/(size of hash map)`) of the data structure. Since the size of the dat structure is fixed, this should never be greater than 1.
 
-// If your language provides a built-in hashing function for strings
-// (ex. `hashCode` in Java or `__hash__` in Python) you are welcome to use that.
-// If not, you are welcome to do something naive, or use something you find online
-// with proper attribution.
-
 type Node struct {
-  key string
-  value interface{}
+	key   string
+	value interface{}
 }
 
 type Hash struct {
-  values [][]Node
-  count int
-  size int
+	values [][]Node
+	count  uint32
+	size   uint32
 }
 
-func New(size int) *Hash {
-  return &Hash {
-    make([][]Node, size),
-    0,
-    size
-  }
+func New(size uint32) (*Hash, error) {
+	if size <= 0 {
+		return nil, errors.New("Size of hash must be a natural number")
+	}
+	return &Hash{
+		make([][]Node, size),
+		0,
+		size,
+	}, nil
 }
 
 func (h *Hash) Set(key string, value interface{}) bool {
-  index := index(key, h.size)
-  for _, node := range h.values[index] {
-    if node.key == key {
-      node.value = value
-      return true
-    }
-  }
-  if(h.count == h.size) {
-    return false
-  }
-  h.values[index] = append(h.values[index], value)
-  h.count++
-  return true
+	index := index(key, h.size)
+	for _, node := range h.values[index] {
+		if node.key == key {
+			node.value = value
+			return true
+		}
+	}
+	if h.count == h.size {
+		return false
+	}
+	h.values[index] = append(h.values[index], Node{key,value})
+	h.count++
+	return true
 }
 
 func (h *Hash) Get(key string) interface{} {
-  index := index(key, h.size)
-  for _, node := range h.values[index] {
-    if node.key == key {
-      return node.value
-    }
-  }
-  return nil
+	index := index(key, h.size)
+	for _, node := range h.values[index] {
+		if node.key == key {
+			return node.value
+		}
+	}
+	return nil
 }
 
 func (h *Hash) Delete(key string) interface{} {
-  index := index(key, h.size)
-  for _, node := range h.values[index] {
-    if node.key == key {
-      h.values[index] = append(h.values[index][:i], h.values[index][i+1:]...)
-      count--
-      return node.value
-    }
-  }
-  return nil
+	index := index(key, h.size)
+	for i, node := range h.values[index] {
+		if node.key == key {
+			h.values[index] = append(h.values[index][:i], h.values[index][i+1:]...)
+			h.count--
+			return node.value
+		}
+	}
+	return nil
 }
 
-func (h *Hash) Load() float {
-  return float(h.count) / float(h.size)
+func (h *Hash) Load() float32 {
+	return float32(h.count) / float32(h.size)
 }
 
-func index(key string, size int) {
-  return int(hash(key) % size)
+func (h *Hash) Count() uint32 {
+	return h.count
+}
+
+func (h *Hash) Size() uint32 {
+	return h.size
+}
+
+func index(key string, size uint32) uint32 {
+	return uint32(hash(key) % size)
 }
 func hash(key string) uint32 {
-  h := fnv.New64a()
-  h.Write([]byte(key))
-  return h.Sum64()
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return h.Sum32()
 }
--- hash_test.go
+++ hash_test.go
@@ -1,25 +1,52 @@
 package hash
 
-import "testing"
-
+import(
+  "testing"
+)
 
 func TestNew(t *testing.T) {
-
+	tables := []struct {
+		input, expected uint32
+	}{
+		{0, 0},
+		{1, 1},
+		{100, 100},
+	}
+	for _, pair := range tables {
+		h, err := New(pair.input)
+		if pair.input <= 0 {
+      if err == nil {
+        t.Errorf("hash.New Expected error with 0 size")
+      }
+		} else {
+      if err != nil {
+        t.Errorf("hash.New unexpected error")
+        continue
+      }
+      if h.Size() != pair.expected {
+        t.Errorf("hash.New incorrect size expected: %d, actual: %d",
+          pair.expected, pair.input)
+      }
+      if h.Count() != 0 {
+        t.Errorf("hash.New incorrect count")
+      }
+    }
+	}
 }
 
 func TestSetNew(t *testing.T) {
 
 }
 
-func TestSetAndUpdate (t *testing.T) {
+func TestSetAndUpdate(t *testing.T) {
 
 }
 
-func TestSetNewFull (t *testing.T) {
+func TestSetNewFull(t *testing.T) {
 
 }
 
-func TestSetAndUpdateFull (t *testing.T) {
+func TestSetAndUpdateFull(t *testing.T) {
 
 }
 
